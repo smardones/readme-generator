99779e7e2ee72e23cc536b06e624ef8c7f1fb6cd
smardone <shaun.mardones@gmail.com>
2020-08-02 14:31:30 -0400

    added readme template

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,106 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateReadMe = require('./utils/generateMarkdown.js');
+
 // array of questions for user
 const questions = [
+    
+    {
+        type: "input",
+        name: "username",
+        message: "What is your GitHub username?"
+    },
+
+    {
+        type: "input",
+        name: "email",
+        massage: "What is your email?"
+    },
+
+    {
+        type: "input",
+        name: "title",
+        message: "What is the project's name?"
+    },
+
+    {
+        type: "input",
+        name: "description",
+        message: "Please describe the project."
+    },
+
+    {
+        type: "input",
+        name: "installation",
+        message: "Please advise how to install the project."
+    },
+
+    {
+        type: "input",
+        name: "usage",
+        message: "Please describe the uses and functionality of the project."
+    },
+
+    {
+        type: "list",
+        name: "license",
+        message: "What is the project's license?",
+        choices: [
+            "MIT",
+            "GNU GPLv3",
+            "Apache",
+            "Unilicense"
+        ]
+    },
+
+    {
+        type: "input",
+        name: "contributing",
+        message: "What are the rules for contributing to the project?"
+    },
+
+    {
+        type: "input",
+        name: "test",
+        message: "Please provide a description of tests for the project."
+    }
 
 ];
 
+
 // function to write README file
-function writeToFile(fileName, data) {
-}
+const writeToFile = (data) => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./dist/readme.md', data, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: 'Readme file created!'
+            });
+    });
+})
+};
 
 // function to initialize program
-function init() {
+const init = () => {
+    console.log("Welcome to the ReadMe Generator! Please answer the below questions:");
+    return inquirer.prompt(questions);
+    
 
 }
 
 // function call to initialize program
-init();
+init()
+.then(readMeData => {
+    return generateReadMe(readMeData)
+})
+.then(resultingMarkDown => 
+    writeToFile(resultingMarkDown))
+.catch(err => {
+    console.log(err);
+});
+
+
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,8 +1,42 @@
 // function to generate markdown for README
+
 function generateMarkdown(data) {
+  
   return `# ${data.title}
+          
+          ## Description
+          ${data.description}
+
+          ## Table of Contents
+          * [Installation](#Installation)
+          * [Usage](#Usage)
+          * [License](#License)
+          * [Contributing](#Contributing)
+          * [Tests](#Tests)
+          * [Questions/Contact](#Questions?)
+
+          ## Installation
+          ${data.installation}
+
+          ## Usage
+          ${data.usage}
+
+          ## License
+          ${data.license}
+
+          ## Contributing
+          ${data.contributing}
+
+          ## Tests
+          ${data.tests}
+
+          ## Questions?
+          Feel free to contact me with question:
+          GitHub - [${data.username}]("https://github.com/${data.username}")
+          Email - [${data.email}]("${data.email}")
 
 `;
 }
+
 
 module.exports = generateMarkdown;
